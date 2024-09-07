257533b35239b87b6f980e95862a4f1bec962113
tarundeepsinghh <tarundeep101010@gmail.com>
2024-09-08 05:09:07 +0530

    fix:landing page not loading issue

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -8,7 +8,7 @@ import {
 import NotFoundPage from "./pages/not-found";
 import { lazy, Suspense } from "react";
 import { Box, CircularProgress } from "@mui/material";
-import { DashboardLayout } from "./layout/dashboard";
+// import { DashboardLayout } from "./layout/dashboard";
 
 function AppRouter() {
   const LandingPage = lazy(() =>
@@ -32,6 +32,8 @@ function AppRouter() {
     createRoutesFromElements(
       <>
         <Route path='/' element={<Navigate to='/coming-soon' replace />} />
+        {/* //temp */}
+        {/* <Route path='/' element={<Navigate to='/landing' replace />} /> */}
         <Route
           path='coming-soon'
           element={
@@ -52,48 +54,48 @@ function AppRouter() {
             </Suspense>
           }
         />
-        <Route path='/' element={<DashboardLayout />}>
-          <Route
-            path='landing'
-            element={
-              <Suspense
-                fallback={
-                  <Box
-                    display='flex'
-                    justifyContent='center'
-                    alignItems='center'
-                    style={{ height: "100vh" }}>
-                    <Box textAlign='center'>
-                      <CircularProgress title='progress loader' />
-                      <p>Loading...</p>
-                    </Box>
+        {/* <Route path='/' element={<DashboardLayout />}> */}
+        <Route
+          path='landing'
+          element={
+            <Suspense
+              fallback={
+                <Box
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  style={{ height: "100vh" }}>
+                  <Box textAlign='center'>
+                    <CircularProgress title='progress loader' />
+                    <p>Loading...</p>
                   </Box>
-                }>
-                <LandingPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path='/health-benefits'
-            element={
-              <Suspense
-                fallback={
-                  <Box
-                    display='flex'
-                    justifyContent='center'
-                    alignItems='center'
-                    style={{ height: "100vh" }}>
-                    <Box textAlign='center'>
-                      <CircularProgress title='progress loader' />
-                      <p>Loading...</p>
-                    </Box>
+                </Box>
+              }>
+              <LandingPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path='/health-benefits'
+          element={
+            <Suspense
+              fallback={
+                <Box
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  style={{ height: "100vh" }}>
+                  <Box textAlign='center'>
+                    <CircularProgress title='progress loader' />
+                    <p>Loading...</p>
                   </Box>
-                }>
-                <HealthBenefitPage />
-              </Suspense>
-            }
-          />
-        </Route>
+                </Box>
+              }>
+              <HealthBenefitPage />
+            </Suspense>
+          }
+        />
+        {/* </Route> */}
         <Route path='*' element={<NotFoundPage />} />
       </>
     ),
